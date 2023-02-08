27c2e566b0e3014dbe442a515ed04f2ca9750eb1
AlexIstra1989 <alex888fcsm@gmail.com>
2023-02-08 14:50:43 +0300

    added contacts-page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -471,7 +471,93 @@ function initSliders() {
 			// Пагинация
 			
 			pagination: {
-				el: '.swiper-pagination-history',
+				el: '.swiper-pagination-hero',
+				clickable: true,
+			},
+			
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.history-button-prev',
+				nextEl: '.history-button-next',
+			},
+
+			// Брейкпоинты
+			
+			breakpoints: {
+				// 320: {
+				// 	slidesPerView: 1,
+				// 	spaceBetween: 0,
+				// 	autoHeight: true,
+				// },
+    // 550: {
+				// 	slidesPerView: 2,
+				// 	spaceBetween: 20,
+				// 	autoHeight: true,
+				// },
+				// 768: {
+				// 	slidesPerView: 3,
+				// 	spaceBetween: 20,
+				// },
+				// 992: {
+				// 	slidesPerView: 3,
+				// 	spaceBetween: 20,
+				// },
+				// 1268: {
+				// 	slidesPerView: 4,
+				// 	spaceBetween: 30,
+				// },
+			},
+			
+			// События
+			on: {
+
+			}
+		});
+	}
+ if (document.querySelector('.subdivisions__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.subdivisions__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 20,
+			autoHeight: true,
+			speed: 500,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			
+			// Эффекты
+			// effect: 'fade',
+   // fadeEffect: {
+   //  crossFade: true
+   // },
+			// autoplay: {
+			// 	delay: 3000,
+			// 	disableOnInteraction: false,
+			// },
+			
+
+			// Пагинация
+			
+			pagination: {
+				el: '.swiper-pagination-subdivisions',
 				clickable: true,
 			},
 			
